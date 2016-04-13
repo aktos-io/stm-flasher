8609882fbcf7bf0dafaf30baf549fdba05732a1f
Efe Can <efecanicoz@gmail.com>
2016-04-13 22:23:34 +0300

    server application to flash the firmware over TCP/IP is added

---
+++ tcp_server.py
@@ -0,0 +1,57 @@
+from aktos_dcs import *
+from stm_flasher import StmFlasher
+from gevent.queue import Queue
+
+
+class ConnectionHandler(TcpHandlerActor):
+	
+    def prepare(self):
+        self.flasher = StmFlasher()
+        self.flasher.setup_phy = self.setup_phy
+        self.flasher.read_phy = self.read_phy
+        self.flasher.write_phy = self.write_phy
+        self.read_queue = Queue()
+        
+        
+    def setup_phy(self):
+        print "Setup the physical connection"
+    
+    def read_phy(self, count=1):
+        chunk = ""
+        while True:
+            chunk += self.read_queue.get()
+            print "Chunk: ", repr(chunk)
+            if len(chunk) == count:
+                break
+                
+        return chunk
+    
+    def write_phy(self, data):
+        print "Sending : ", repr(data)
+        self.socket_write(data)    
+    
+    def on_connect(self):
+        print "there is a connection!", self.client_id
+        print "Preparig to connect stm"
+        self.flasher.setup_hardware()
+        self.flasher.wait_setup_hardware()
+        self.flasher.upload_firmware('./ch.bin')
+        if self.flasher.upload_successful():
+            print "Upload is succesful"
+        else:
+            print "Problem while uploading firmware"
+        
+    def on_disconnect(self):
+        print "client disconnected: ", self.client_id
+
+    def socket_read(self, data):
+        print "I got following data: ", repr(data)
+        self.read_queue.put(data)
+
+    def action(self):
+        pass
+
+print "Started TcpServer on port 22334"
+#TcpServerActor(address='0.0.0.0', port=22334)
+TcpServerActor(address='0.0.0.0', port=22334, handler=ConnectionHandler)
+wait_all()
