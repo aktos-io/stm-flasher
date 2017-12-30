771627bb9c3a4cd572ec33524f2a9ce0fdd1aa3a
Cerem Cem ASLAN <cem@aktos.io>
2017-12-30 19:19:05 +0300

    cleanup

--- stm_mass_erase.py
+++ stm_mass_erase.py
@@ -1,45 +1,51 @@
-#Look AN3155 for full usart protocol
-#http://www.st.com/content/ccc/resource/technical/document/application_note/51/5f/03/1e/bd/9b/45/be/CD00264342.pdf/files/CD00264342.pdf/jcr:content/translations/en.CD00264342.pdf
+#!/usr/bin/python
+
+"""
+Mass erase over USART1 while boot0 is wired to VDD while powering up
+
+
+See AN3155 for full usart protocol
+http://www.st.com/content/ccc/resource/technical/document/application_note/51/5f/03/1e/bd/9b/45/be/CD00264342.pdf/files/CD00264342.pdf/jcr:content/translations/en.CD00264342.pdf
+"""
 
 import serial
 from os import stat
 
-serial_desc = None
+print("1. Assuming serial port on host is: /dev/ttyUSB0")
+print("2. Assuming correct Boot0 pin of the MCU is connected to VDD while powering up")
+print("3. Assuming correct MCU.USART port (USART1) is being used.")
+print('(Refer the AN3155 for further details)')
+print "----------------------------------------------------------------"
 
-print("Assuming device mounted on:/dev/ttyUSB0")
-print("Assuming correct boot mode is selected via boot pins")
+# TODO: add an automated finder or take from user
+s = serial.Serial()
+s.port = "/dev/ttyUSB0"
+s.baudrate = 115200
+s.parity=serial.PARITY_EVEN
+s.timeout = 2  # seconds
+s.open()
 
-#add an automated finder or take from user
-serial_desc = serial.Serial("/dev/ttyUSB0",115200,parity=serial.PARITY_EVEN)
-if serial_desc.is_open == False:
-	print("Cannot open serial port control the connection")
-	exit(-4)
+if not s.is_open:
+    print("Cannot open serial port control the connection")
+    exit(-4)
 
 print("Initializing connection")
-serial_desc.write("\x7f")
-
-ans = serial_desc.read()
-
-if ans != "\x79":
-	print("Cannot initialize connection, be sure you use correct usart port")
+s.write("\x7f")
+if s.read() != "\x79":
+	print("Cannot initialize connection with the MCU")
 	exit(-1)
-
 print("Connection initialized")
 
-serial_desc.write("\x43\xbc")
-ans = serial_desc.read()
-
-if ans != "\x79":
+print "Initiating erase function..."
+s.write("\x43\xbc")
+if s.read() != "\x79":
 	print("Cannot initiate erase functionality")
 	exit(-2)
 
-print("Erase function is called")
-
-serial_desc.write("\xff\x00")
-ans = serial_desc.read()
-if ans != "\x79":
+print("Calling erase function...")
+s.write("\xff\x00")
+if s.read() != "\x79":
 	print("Something went wrong while erasing")
 	exit(-3)
 
 print("All flash is erased, bye")
-
